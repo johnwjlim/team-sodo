d86b4e5cf8cafea3bd08a5740d3040687aa1b721
John Lim <wjlim@uw.edu>
2019-04-28 16:30:16 -0700

    update git ignore

---
+++ landing-page/public/styles-0a6c738c5a13f53bab90.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{294:function(n,o,w){},295:function(n,o,w){},599:function(n,o,w){}}]);
+//# sourceMappingURL=styles-0a6c738c5a13f53bab90.js.map
---
+++ landing-page/public/styles-a484b2a77eb08d7c533a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{293:function(n,o,w){},294:function(n,o,w){},599:function(n,o,w){}}]);
+//# sourceMappingURL=styles-a484b2a77eb08d7c533a.js.map
