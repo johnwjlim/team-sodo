d2f83902708ee5b621e992181878bcdaaa9d1b38
John Lim <wjlim@uw.edu>
2019-06-02 12:26:48 -0700

    added functionality for appending data based on parsed CSV

--- react-app/src/pages/resources/dialysis-test.jsx
+++ react-app/src/pages/resources/dialysis-test.jsx
@@ -16,7 +16,7 @@ import Map from '../../components/resources/map'
 // import Map from '../../components/resources/dialysis/dialysis-map'
 import Panel from '../../components/resources/dialysis/dialysis-panel'
 
-import { getDialysisData, getCancerData, getEMContacts } from '../../components/resources/parse'
+import { getDialysisData } from '../../components/resources/parse'
 
 const TOKEN = 'pk.eyJ1Ijoid2psaW0iLCJhIjoiY2plNGtpMXFpNmw3ZTMzcXA4a3l1NmdwOSJ9.2Ou7bageJ-DCfiASBrV5HA';
 const mbxGeocode = require('@mapbox/mapbox-sdk/services/geocoding');
@@ -44,7 +44,7 @@ export default function Dialysis() {
   const dialysisState = useSelector(state => state.dialysisReducer)
 
   useEffect(() => {
-    return () => {
+    return function cleanup() {
       dispatch({type: UNMOUNT_DIALYSIS})
       dispatch({type: RESET_VIEWPORT})
     }
@@ -56,34 +56,63 @@ export default function Dialysis() {
 
   useEffect(() => {
     async function fetchData() {
-      // let dialysisData = await getDialysisData();
-      // let parsedKeys = dialysisData.map((object, index) => {
-      //   return object.facilityName
-      // })
-
       ref.on('value', async snapshot => {
         let value = snapshot.val()
-        let databaseKeys = value.map((object) => {
-          return object.facilityName
-        })
-        // if (arraysMatch(parsedKeys, databaseKeys)) {
-        //   let array = await checkCoordinates(value)
-        //   ref.set(array)
-        //   dispatch({type: "SET_DIALYSIS_DATA", payload: value})
-        // } else {
-        //   // doesn't actually do anything yet
-        //   appendData()
-        // }
+        if (!checkCoordinates(value)) {
+          let updatedArray = await getCoordinates(value)
+          ref.set(updatedArray);
+        }
         dispatch({type: "SET_DIALYSIS_DATA", payload: value})
       })
     }
     fetchData();
   },[])
 
-  function appendData() {
+  /**
+   * This effect runs on initial component mount and appends the database if necessary
+   */
+  useEffect(() => {
+    appendData()
+  }, [])
 
+  /**
+   * This function parses the source CSV and compares the result to database contents. 
+   * Function will rewrite the database at the relevant ref if parsed data does not match data contained in database.
+   * This function should only be executed once on component mount
+   */
+  function appendData() {
+    ref.once('value').then( async snapshot => {
+      let parse = await getDialysisData()
+      if (parse.length > 0) {
+        if (!arraysMatch(parse, snapshot.val())) {
+          // TODO: rewrite databse
+          ref.set(parse)
+        }
+      }
+    })
   }
 
+  /**
+   * Checks if objects in array have coordinates. Returns false if an object does not have coordinates
+   * 
+   * @param {Array} data - array of listing objects 
+   * @return {Boolean}
+   */
+  async function checkCoordinates(data) {
+    for (var object of data) {
+      if (!object.hasOwnProperty('coords')) {
+        return false
+      }
+    }
+    return true;
+  }
+
+  /**
+   * This function requests coordinates of a given location using Mapbox's Forward Geocode Service
+   * 
+   * @param {String} string - Address or landmark to query
+   * @return {Array} - Returns coordinates in an array: [longitude, latitidue]
+   */
   async function requestCoordinates(string) {
     return await geocodeService.forwardGeocode({
       query: string,
@@ -95,7 +124,12 @@ export default function Dialysis() {
     })
   }
 
-  async function checkCoordinates(data) {
+  /**
+   * 
+   * @param {Array} data - Array of listing objects  
+   * @return {Array} - Array of listing objects with updated coordinates from Mapbox
+   */
+  async function getCoordinates(data) {
     return await Promise.all(data.map(async (object) => {
       if (!object.hasOwnProperty('coords')) {
         let query = object.addressLine1 + " " + object.Zip
@@ -109,25 +143,13 @@ export default function Dialysis() {
     }))
   }
 
-  function handleData(parsedArray, databaseArray) {
-    if (arraysMatch(databaseArray, parsedArray)) {
-      // handle coordinates
-
-    } else {
-
-      let existingFacilities = databaseArray.map((object, index) => {
-        return object.facilityName
-      })
-
-      let newStuff = parsedArray;
-      newStuff.forEach((object, index) => {
-        if (existingFacilities.includes(object.facilityName)) {
-          newStuff.splice(index, 1)
-        }
-      })
-    }
-  }
-
+  /**
+   * Iteratively compares 2 arrays to see determine if they match. Returns true if arrays match. 
+   * 
+   * @param {Array} arr1 - First array to be compared
+   * @param {Array} arr2 - Second array to be compared
+   * @return {Boolean} - Returns true if arrays match, false if arrays do not match
+   */
   function arraysMatch(arr1, arr2) {
     if (arr1.length !== arr2.length) {
       return false;
